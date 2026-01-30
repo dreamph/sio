13bd4f918e9874fddb4fbd8d6ee2fca707bfc325
dreamph <dream_ph@hotmail.com>
2026-01-30 17:43:16 +0700

    cleanup

--- fio/mmap_stub.go
+++
@@ -1,9 +0,0 @@
-//go:build !darwin && !linux && !freebsd && !netbsd && !openbsd
-
-package fio
-
-import "os"
-
-func tryMmap(_ *os.File, _ int64) ([]byte, func() error, bool) {
-	return nil, nil, false
-}
